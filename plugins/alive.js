03260c75f6d7618dd942c503bf818ec5b0eb0ae4
Achiya CT <89849687+CTAchiya@users.noreply.github.com>
2021-09-01 09:33:48 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -6,6 +6,6 @@ let FM = Config.WORKTYPE == 'public' ? false : true
 
 XTroid.addCMD({pattern: 'alive', fromMe: FM, deleteCommand: true,}, (async (message, match) => {
 
-var lasi = await axios.get(`${Config.ALIVEIMG}`, { responseType: 'arraybuffer' })
+var lasi = await axios.get(`https://telegra.ph/file/bb314c6f8f29c8d1d0ee2.jpg`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid, Buffer.from(lasi.data), MessageType.image, {mimetype: Mimetype.png, caption: '            '+Config.ALIVETEXT+'        ⚡🇱🇰🔰✥▬ALPHA▬✥🔰🇱🇰⚡'})
     }));
